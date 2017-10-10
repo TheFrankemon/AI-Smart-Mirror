db7082d800d96c2e3a7dcb3c64d16132540defc6
Franco Montiel <francomontiel@hotmail.com>
2017-10-10 01:47:01 -0400

    Fixed TAB indentation

--- bot.py
+++ bot.py
@@ -20,121 +20,121 @@ debugger_enabled = True
 camera = 0
 
 class Bot(object):
-    def __init__(self):
-        with open('config.json') as data_file:
-	    conf = json.load(data_file)
-        self.nlg = NLG(user_name=user_name)
-	self.speech = Speech(debugger_enabled=debugger_enabled)
-	self.vision = Vision(camera=camera)
-        self.mongo = Mongo()
+	def __init__(self):
+		with open('config.json') as data_file:
+			conf = json.load(data_file)
+		self.nlg = NLG(user_name=user_name)
+		self.speech = Speech(debugger_enabled=debugger_enabled)
+		self.vision = Vision(camera=camera)
+		self.mongo = Mongo()
 
-    def start(self):
-	"""
-	Main loop.
-	:return:
-	"""
-        while True:
-            requests.get("http://localhost:8888/clear")
-            if self.vision.recognize_face('c1.png'):
-		print "Found face > Took Photo#1"
-                self.__intro_action()
-		self.vision.recognize_face('c2.png')
-		############## set timer to wait for another face
-		print "Found face > Took Photo#2"
-		self.__user_name_action() #########3 here it must enable keyboard, wait for ACCEPT
-		self.mongo.add(user_name,
-                    "/home/pi/AI-Smart-Mirror-Franco/img/c1.png",
-                    "/home/pi/AI-Smart-Mirror-Franco/img/c2.png")
-		print "Found face > Took Photo#2"
-		self.__acknowledge_action()
-		####### it should return Y/N...Y > decide action...N > appreciation action
-		self.decide_action()
+	def start(self):
+		"""
+		Main loop.
+		:return:
+		"""
+		while True:
+			requests.get("http://localhost:8888/clear")
+			if self.vision.recognize_face('c1.png'):
+				print "Found face > Took Photo#1"
+				self.__intro_action()
+				self.vision.recognize_face('c2.png')
+				############## set timer to wait for another face
+				print "Found face > Took Photo#2"
+				self.__user_name_action() #########3 here it must enable keyboard, wait for ACCEPT
+				self.mongo.add(user_name,
+					"/home/pi/AI-Smart-Mirror-Franco/img/c1.png",
+					"/home/pi/AI-Smart-Mirror-Franco/img/c2.png")
+				print "Found face > Took Photo#2"
+				self.__acknowledge_action()
+				####### it should return Y/N...Y > decide action...N > appreciation action
+				self.decide_action()
 
-    def decide_action(self):
-	"""
-	Recursively decides an action based on the intent.
-	:return:
-	"""
-	recognizer, audio = self.speech.listen_for_audio()
+	def decide_action(self):
+		"""
+		Recursively decides an action based on the intent.
+		:return:
+		"""
+		recognizer, audio = self.speech.listen_for_audio()
 
-	# received audio data, now we'll recognize it using Google Speech Recognition
-	#speech = self.speech.google_speech_recognition(recognizer, audio)
+		# received audio data, now we'll recognize it using Google Speech Recognition
+		#speech = self.speech.google_speech_recognition(recognizer, audio)
 
-	# received audio data, now we'll recognize it using Wit Speech API
-        speech = self.speech.wit_speech_recognition(recognizer, audio, str(conf["tokens"]["wit_ai_token"]))
+		# received audio data, now we'll recognize it using Wit Speech API
+		speech = self.speech.wit_speech_recognition(recognizer, audio, str(conf["tokens"]["wit_ai_token"]))
 
-	if speech is not None:
-            try:
-                ## Uncomment for HARDCODED SPEECH ##
-                #speech = "torta UPB"
-                print 'Requesting WIT.AI [' + speech + ']'
-                r = requests.get('https://api.wit.ai/message?v=20170403&q=%s' % speech, headers={'Authorization': str(conf["tokens"]["wit_ai_token"])})
-                print 'Text ' + r.text
-                #print r.headers['authorization']
-                json_resp = json.loads(r.text)
-                entities = None
-                intent = None
-		if 'entities' in json_resp and 'Intent' in json_resp['entities']:
-                    entities = json_resp['entities']
-                    intent = json_resp['entities']['Intent'][0]["value"]
+		if speech is not None:
+			try:
+				## Uncomment for HARDCODED SPEECH ##
+				#speech = "torta UPB"
+				print 'Requesting WIT.AI [' + speech + ']'
+				r = requests.get('https://api.wit.ai/message?v=20170403&q=%s' % speech, headers={'Authorization': str(conf["tokens"]["wit_ai_token"])})
+				print 'Text ' + r.text
+				#print r.headers['authorization']
+				json_resp = json.loads(r.text)
+				entities = None
+				intent = None
+				if 'entities' in json_resp and 'Intent' in json_resp['entities']:
+					entities = json_resp['entities']
+					intent = json_resp['entities']['Intent'][0]["value"]
 
-                print intent
-                if intent == 'appearance':
-                    self.__appearance_action()
-                elif intent == 'insult':
-                    self.__insult_action()
-                    return
-                elif intent == 'appreciation':
-                    self.__appreciation_action()
-                    return
-                else: # No recognized intent
-                    self.__text_action("Perdón, aún estoy en kinder.")
+				print intent
+				if intent == 'appearance':
+					self.__appearance_action()
+				elif intent == 'insult':
+					self.__insult_action()
+					return
+				elif intent == 'appreciation':
+					self.__appreciation_action()
+					return
+				else: # No recognized intent
+					self.__text_action("Perdón, aún estoy en kinder.")
 
-            except Exception as e:
-		print "Failed wit!"
-		print(e)
-		traceback.print_exc()
-		self.__text_action("Perdón, no te entendí")
-		return
+			except Exception as e:
+				print "Failed wit!"
+				print(e)
+				traceback.print_exc()
+				self.__text_action("Perdón, no te entendí")
+				return
 
-            self.decide_action()
+			self.decide_action()
 
-    # CUSTOM
-    def __intro_action(self):
-        intro = self.nlg.intro()
+	# CUSTOM
+	def __intro_action(self):
+		intro = self.nlg.intro()
 
-        if intro is not None:
-            self.__text_action(intro)
-        else:
-            self.__text_action("Me raye, contacta a un humano por favor")
+		if intro is not None:
+			self.__text_action(intro)
+		else:
+			self.__text_action("Me raye, contacta a un humano por favor")
 
-    def __user_name_action(self):
-        if self.nlg.user_name is None:
-            self.__text_action("I don't know your name. You can configure it in bot.py")
+	def __user_name_action(self):
+		if self.nlg.user_name is None:
+			self.__text_action("I don't know your name. You can configure it in bot.py")
 
-        self.__text_action(user_name)
+		self.__text_action(user_name)
 
-    def __acknowledge_action(self):
-        acknowledge = self.nlg.acknowledge()
+	def __acknowledge_action(self):
+		acknowledge = self.nlg.acknowledge()
 
-        if acknowledge is not None:
-            self.__text_action(acknowledge)
-        else:
-            self.__text_action("Me raye, contacta a un humano por favor")
+		if acknowledge is not None:
+			self.__text_action(acknowledge)
+		else:
+			self.__text_action("Me raye, contacta a un humano por favor")
 
-    def __appreciation_action(self):
-        self.__text_action(self.nlg.appreciation())
+	def __appreciation_action(self):
+		self.__text_action(self.nlg.appreciation())
 
-    def __insult_action(self):
-        self.__text_action(self.nlg.insult())
+	def __insult_action(self):
+		self.__text_action(self.nlg.insult())
 
-    def __appearance_action(self):
-        requests.get("http://localhost:8888/face")
+	def __appearance_action(self):
+		requests.get("http://localhost:8888/face")
 
-    def __text_action(self, text=None):
-        if text is not None:
-            requests.get("http://localhost:8888/statement?text=%s" % text)
-            self.speech.synthesize_text(text)
+	def __text_action(self, text=None):
+		if text is not None:
+			requests.get("http://localhost:8888/statement?text=%s" % text)
+			self.speech.synthesize_text(text)
 
 if __name__ == "__main__":
 	bot = Bot()
--- mongo.py
+++ mongo.py
@@ -10,45 +10,45 @@ dbname = ""
 db = None
 
 class Mongo(object):
-    def __init__(self):
-        with open('config.json') as data_file:
-            conf = json.load(data_file)
-        global usr, pwd, serverip, dbname
-        usr = str(conf["conn"]["usr"])
-        pwd = str(conf["conn"]["pwd"])
-        serverip = str(conf["conn"]["serverip"])
-        dbname = str(conf["conn"]["dbname"])
-        self.connect()
+	def __init__(self):
+		with open('config.json') as data_file:
+			conf = json.load(data_file)
+		global usr, pwd, serverip, dbname
+		usr = str(conf["conn"]["usr"])
+		pwd = str(conf["conn"]["pwd"])
+		serverip = str(conf["conn"]["serverip"])
+		dbname = str(conf["conn"]["dbname"])
+		self.connect()
 
-    def connect(self):
-        try:
-            concat = "mongodb://" + usr + ":" + pwd + "@" + serverip + "/" + dbname        
-            print("Connecting to DB:..." + concat)
-            client = pymongo.MongoClient(concat)
-            global db
-            db = client.mean
-            print("There are " + str(db.clients.count()) + " on the DB")
+	def connect(self):
+		try:
+			concat = "mongodb://" + usr + ":" + pwd + "@" + serverip + "/" + dbname        
+			print("Connecting to DB:..." + concat)
+			client = pymongo.MongoClient(concat)
+			global db
+			db = client.mean
+			print("There are " + str(db.clients.count()) + " on the DB")
 
-        except Exception as e:
-            print "Failed connection to DB!"
-            print(e)
-            traceback.print_exc()
-            return
+		except Exception as e:
+			print "Failed connection to DB!"
+			print(e)
+			traceback.print_exc()
+			return
 
-    def add(self, name, img_url1, img_url2):
-        #binaryimg = Binary(open("/home/pi/AI-Smart-Mirror-Franco/img/c1.png",'rb').read())
-        try:
-            binaryimg1 = Binary(open(img_url1,'rb').read())
-            binaryimg2 = Binary(open(img_url2,'rb').read())
-            global db
-            db.clients.insert_one({
-                "name":"Juan Doe",
-                "img1":binaryimg1,
-                "img2":binaryimg2
-            })
+	def add(self, name, img_url1, img_url2):
+		#binaryimg = Binary(open("/home/pi/AI-Smart-Mirror-Franco/img/c1.png",'rb').read())
+		try:
+			binaryimg1 = Binary(open(img_url1,'rb').read())
+			binaryimg2 = Binary(open(img_url2,'rb').read())
+			global db
+			db.clients.insert_one({
+				"name":"Juan Doe",
+				"img1":binaryimg1,
+				"img2":binaryimg2
+			})
 
-        except Exception as e:
-            print "Failed to parse images"
-            print(e)
-            traceback.print_exc()
-            return
+		except Exception as e:
+			print "Failed to parse images"
+			print(e)
+			traceback.print_exc()
+			return
