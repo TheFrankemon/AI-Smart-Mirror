b0cbd716788d193ae2dd579042d7658bc4cb7066
TheFrankemon <francomontiel@hotmail.com>
2018-02-21 12:35:17 -0400

    Added 'comment' field to user object

--- firebase.py
+++ firebase.py
@@ -44,7 +44,7 @@ class Firebase(object):
 			storage.child(filename).put(imgpath)
 			img2url = storage.child(filename).get_url(None)
 
-			data = {"img1": img1url, "img2": img2url, "isCompleted": False, "name": name, "ts": timestamp, "user": "", "userUID": ""}
+			data = {"img1": img1url, "img2": img2url, "isCompleted": False, "name": name, "ts": timestamp, "user": "", "userUID": "", "comment": ""}
 			db.child("clients").push(data)
 
 		except Exception as e:
